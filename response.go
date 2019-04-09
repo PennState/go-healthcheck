9f8d5d2ee973fd7590543df573b54b0a99b266e4
Steve Moyer <smoyer@psu.edu>
2019-04-09 16:49:00 -0400

    Changed the package to match the project name

--- response.go
+++ response.go
@@ -1,8 +1,8 @@
-package health
+package healthcheck
 
 import "net/http"
 
-func GetHealthHandler(checks ...Check) http.HandlerFunc {
+func GetHealthHandler(checkers ...Checker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//TODO: iterate over checks and determine overall status
 		//      return handler function
