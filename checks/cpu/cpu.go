2589b5bc54e32c7f95c8fe89c17aa056bda49dd2
Steve Moyer <smoyer@psu.edu>
2019-04-12 15:58:19 -0400

    Adds a Check that reads CPU usage from /proc/stat

---
+++ checks/cpu/cpu.go
@@ -0,0 +1,41 @@
+package cpu
+
+import (
+	linuxproc "github.com/c9s/goprocinfo/linux"
+	"github.com/selesy/go-healthcheck"
+	log "github.com/sirupsen/logrus"
+)
+
+type CPUCheck struct {
+}
+
+//See: https://github.com/c9s/goprocinfo
+//See: https://www.linuxhowtos.org/System/procstat.htm
+func Check() ([]healthcheck.Check, healthcheck.Status) {
+	var checks []healthcheck.Check
+
+	stat, err := linuxproc.ReadStat("/proc/stat")
+	if err != nil {
+		return checks, healthcheck.Warn
+	}
+
+	log.Info(stat.CPUStats[0])
+
+	for _, s := range stat.CPUStats {
+		log.Info("-----")
+		log.Info("Id: ", s.Id)
+		log.Info("User: ", s.User)
+		log.Info("Nice: ", s.Nice)
+		log.Info("System: ", s.System)
+		log.Info("Idle: ", s.Idle)
+		log.Info("IOWait: ", s.IOWait)
+		log.Info("IRQ: ", s.IRQ)
+		log.Info("Soft IRQ: ", s.SoftIRQ)
+		log.Info("Steal: ", s.Steal)
+		log.Info("Guest: ", s.Guest)
+		log.Info("Guest nice: ", s.GuestNice)
+	}
+	log.Info("-----")
+
+	return checks, healthcheck.Pass
+}
