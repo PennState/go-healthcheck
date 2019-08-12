0aaab686a912d1f278582e59f003415beb3aca0a
Nathan Bowler <nzb301@psu.edu>
2019-08-12 11:41:34 -0400

    Marshal and write response in GetHealthHandler

--- pkg/health/response.go
+++ pkg/health/response.go
@@ -1,6 +1,7 @@
 package healthcheck
 
 import (
+	"encoding/json"
 	"net/http"
 
 	log "github.com/sirupsen/logrus"
@@ -8,13 +9,33 @@ import (
 
 func GetHealthHandler(checkers ...Checker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var checks Checks
+		checks := Checks{}
 		status := Pass
 		for _, checker := range checkers {
 			c, s := checker.Check()
 			checks.AddChecks(c...)
 			status = status.Max(s)
 		}
-		log.WithField("Status", status).Debug("Checks: ", checks)
+
+		resp, err := json.Marshal(checks)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			errMsg := err.Error()
+			_, writeError := w.Write([]byte(errMsg))
+			if writeError != nil {
+				log.WithError(writeError).WithContext(r.Context()).WithField("Status", status).Errorf("Unable to write healthcheck error %v", err)
+			} else {
+				log.WithError(err).WithContext(r.Context()).WithField("Status", status).Error("Unable to marshal checks")
+			}
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status.StatusCode())
+		_, err = w.Write(resp)
+		if err != nil {
+			log.WithError(err).WithContext(r.Context()).WithField("Status", status).Error("Unable to write healthcheck response")
+		}
+		log.WithContext(r.Context()).WithField("Status", status).Debug("Checks: ", checks)
 	}
 }
